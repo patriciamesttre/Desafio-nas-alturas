f220b5723b78f24f38134f6ad2cce87e4d85b84f
patriciamesttre <patriciamesttre@hotmail.com>
2019-05-10 20:00:29 -0300

    Gerando obstaculo infinitamente

---
+++ desafio nas alturas/Assets/Scripts/GeradorDeObstaculo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeradorDeObstaculo : MonoBehaviour
+{
+
+
+    [SerializeField]
+
+    private float tempoParaGerar = 3f;
+    [SerializeField]
+
+    private GameObject manualDeInstrucoes;
+
+    private float cronometro = 0f;
+
+    private void Awake()
+    {
+        cronometro = tempoParaGerar;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cronometro -= Time.deltaTime;
+        if (cronometro <= 0)
+        {
+            //Debug.Log("3 segundos");
+            //Debug.Log(Quaternion.identity);
+            GameObject.Instantiate(manualDeInstrucoes, transform.position, Quaternion.identity);
+            cronometro = tempoParaGerar;
+        }
+    }
+}
